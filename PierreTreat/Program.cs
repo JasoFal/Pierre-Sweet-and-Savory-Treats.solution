5fbc69a56d3f7966e35019865224f320e38b17f6
Jason Falk <jasonfalk9@gmail.com>
2024-05-15 11:31:19 -0700

    Add authorization and authentication functionality.

--- PierreTreat/Program.cs
+++ PierreTreat/Program.cs
@@ -1,32 +1,58 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ProjectName.Models;
+using PierreTreat.Models;
+using Microsoft.AspNetCore.Identity;
 
-namespace ProjectName
+namespace PierreTreat
 {
   class Program
   {
-    static void Main(string[] arg)
+    static void Main(string[] args)
     {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(arg);
+
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
       builder.Services.AddControllersWithViews();
-      builder.Services.AddDbContext<ProjectNameContext>(
-        dbContextOptions => dbContextOptions
-          .UseMySql(
-            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-          )
-        )
-      );
+
+      builder.Services.AddDbContext<PierreTreatContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+      
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<PierreTreatContext>()
+                .AddDefaultTokenProviders();
+
+      builder.Services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequiredUniqueChars = 0;
+      });
+
       WebApplication app = builder.Build();
+
       // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
       app.UseStaticFiles();
+
       app.UseRouting();
+
+      app.UseAuthentication(); 
+      app.UseAuthorization();
+
       app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
       app.Run();
     }
   }
