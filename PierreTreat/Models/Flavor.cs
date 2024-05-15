58a75e1644b1f9d594bfbef608bc23c1bbe0c8f7
Jason Falk <jasonfalk9@gmail.com>
2024-05-15 13:49:49 -0700

    Add flavor model.

---
+++ PierreTreat/Models/Flavor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PierreTreat.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+    [Required(ErrorMessage = "Flavor type can't be empty.")]
+    public string Favor { get; set; }
+    public List<TreatFlavor> JoinEntities { get; set; }
+  }
+}
