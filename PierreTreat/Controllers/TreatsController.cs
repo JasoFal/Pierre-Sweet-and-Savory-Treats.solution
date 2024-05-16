25093b4ce2c1ad9334cb5386fefd23b0d23485ac
Jason Falk <jasonfalk9@gmail.com>
2024-05-16 14:51:37 -0700

    Add index route.

---
+++ PierreTreat/Controllers/TreatsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierreTreat.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierreTreat.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierreTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, PierreTreatContext db)
+    {
+      _db = db;
+      _userManager = userManager;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+  }
+}
