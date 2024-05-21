68093d4b11fd04bc1dde398b953f830572fdaea9
Jason Falk <jasonfalk9@gmail.com>
2024-05-21 10:32:57 -0700

    Add route to register page with account register functionality.

--- PierreTreat/Controllers/AccountController.cs
+++ PierreTreat/Controllers/AccountController.cs
@@ -23,5 +23,36 @@ namespace PierreTreat.Controllers
     {
       return View();
     }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
+    }
   }
 }
