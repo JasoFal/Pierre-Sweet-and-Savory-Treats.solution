0e34579039fc0537212957dbd3899437b180e292
Jason Falk <jasonfalk9@gmail.com>
2024-05-21 14:30:18 -0700

    Add route with create flavor functionality.

--- PierreTreat/Controllers/FlavorsController.cs
+++ PierreTreat/Controllers/FlavorsController.cs
@@ -1,24 +1,59 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PierreTreat.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Rendering;
-
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 namespace PierreTreat.Controllers
 {
   public class FlavorsController : Controller
   {
     private readonly PierreTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FlavorsController(PierreTreatContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager, PierreTreatContext db)
     {
       _db = db;
+      _userManager = userManager;
     }
 
     public ActionResult Index()
     {
       return View(_db.Flavors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Flavor flavor)
+    {
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      if (currentUser != null)
+      {
+        if (!ModelState.IsValid)
+        {
+          return View(flavor);
+        }
+        else
+        {
+          _db.Flavors.Add(flavor);
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+      }
+      else
+      {
+        return View(flavor);
+      }
     }
   }
 }
