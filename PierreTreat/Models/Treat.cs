baa9307bc1356a9a1259da8dd9f67ab409f461af
Jason Falk <jasonfalk9@gmail.com>
2024-05-21 14:28:26 -0700

    Fix typo.

--- PierreTreat/Models/Treat.cs
+++ PierreTreat/Models/Treat.cs
@@ -6,7 +6,7 @@ namespace PierreTreat.Models
   public class Treat
   {
     public int TreatId { get; set; }
-    [Required(ErrorMessage = "Treat name can't be empty.")]
+    [Required(ErrorMessage = "Treat type can't be empty.")]
     public string TreatType { get; set; }
     public List<TreatFlavor> JoinEntities { get; set; }
   }
